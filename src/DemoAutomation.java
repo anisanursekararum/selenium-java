b45303a3d146ccb0f0b2c28d5fa59ea17c20dc70
anisanursekararum <anisanursekararum@gmail.com>
2023-06-19 21:34:18 +0700

    add firefox and edge drivers

--- src/DemoAutomation.java
+++ src/DemoAutomation.java
@@ -1,12 +1,18 @@
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DemoAutomation {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "C:\\browserdrivers\\geckodriver.exe");
+		System.setProperty("webdriver.edge.driver", "C:\\browserdrivers\\msedgedriver.exe");
 
-		ChromeDriver driver = new ChromeDriver();
+		//FirefoxDriver driver = new FirefoxDriver();
+		EdgeDriver driver = new EdgeDriver();
+		//ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 	}
 
